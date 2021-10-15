a5c242e894033c56e3b15063aacc3ce8adf1da7f
Jaikishore <jaikishore1997@gmail.com>
2021-10-15 13:15:00 +0530

    Load to New Scene and Back Button Implementation in New Scene

---
+++ Assets/Scripts/FirstScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FirstScene : MonoBehaviour
+{
+    public Button button1,button2,button3;
+    public string Scene1,Scene2,Scene3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button1.onClick.AddListener(LoadScene1);
+        button2.onClick.AddListener(LoadScene2);
+        button3.onClick.AddListener(LoadScene3);
+    }
+
+    private void LoadScene1(){
+        SceneManager.LoadScene(Scene1);
+    }
+
+    private void LoadScene2(){
+        SceneManager.LoadScene(Scene2);
+    }
+    private void LoadScene3(){
+        SceneManager.LoadScene(Scene3);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SwitchTOMain.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SwitchTOMain : MonoBehaviour
+{
+    public Button backBtn;
+    public string SceneName="FirstScene";
+    // Start is called before the first frame update
+    void Start()
+    {
+        backBtn.onClick.AddListener(BackToMain);
+    }
+    public void BackToMain(){
+        SceneManager.LoadScene(SceneName);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
